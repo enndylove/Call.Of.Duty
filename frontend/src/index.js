719497d15cde947a8d33880eec1884f178ec5477
enndy <91780685+enndylove@users.noreply.github.com>
2024-11-28 09:23:09 +0200

    add routes for project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {paths_} from "./config";
+
+import Home from "./pages/Home";
+import Creations from "./pages/Creations";
+import Logs from "./pages/Logs";
+import Prowess from "./pages/Prowess";
+import Skillsets from "./pages/Skillsets";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        {/* ALL ROUTES */}
+        <Route path={paths_.START_PATH}>
+          {/* Index page(home page) */}
+          <Route index element={<Home />}/>
+
+          {/* Creations page */}
+          <Route path={paths_.CREATIONS} element={<Creations />} />
+
+          {/* Data-logs page */}
+          <Route path={paths_.LOGS} element={<Logs />} />
+
+          {/* Prowess page */}
+          <Route path={paths_.PROWESS} element={<Prowess />} />
+
+          {/* Skillsets page */}
+          <Route path={paths_.SKILL_SETS} element={<Skillsets />} />
+        </Route>
+      </Routes>
+  </BrowserRouter>
 );
